d98c559eb6dbd91b29b6817bfe6cd5a7b964971f
Jose Gramajo <gra22907@uvg.edu.gt>
2023-09-07 18:32:19 -0600

    Agregado diseño de RecipePage en la clase RecipesList

--- app/src/main/java/com/group5/recipeapp/presentation/recipes/RecipesList.kt
+++ app/src/main/java/com/group5/recipeapp/presentation/recipes/RecipesList.kt
@@ -4,8 +4,69 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.group5.recipeapp.model.FoodCategoriesEnum
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.group5.recipeapp.R
 
 @Composable
-fun RecipesList(navController: NavHostController, foodCategory: FoodCategoriesEnum) {
-    Text(text = foodCategory.displayableText)
+fun RecipePage(recipe: Recipe) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Sección de la foto de la receta
+        Image(
+            painter = painterResource(id = recipe.imageResourceId),
+            contentDescription = "Recipe Image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(shape = RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        // Sección de los pasos de preparación
+        Text(
+            text = "Pasos de preparación:",
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Text(
+            text = recipe.preparationSteps,
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 16.sp
+            )
+        )
+
+        // Sección de los ingredientes
+        Text(
+            text = "Ingredientes:",
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Text(
+            text = recipe.ingredients,
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 16.sp
+            )
+        )
+    }
 }
