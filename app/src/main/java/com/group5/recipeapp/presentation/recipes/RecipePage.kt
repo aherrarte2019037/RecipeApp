1af0894581118a012e92d6865f45d8c220cd9502
José Gramajo <gra22907@uvg.edu.gt>
2023-11-18 21:28:00 -0600

    Funcion de retorno en RecipePage

--- app/src/main/java/com/group5/recipeapp/presentation/recipes/RecipePage.kt
+++ app/src/main/java/com/group5/recipeapp/presentation/recipes/RecipePage.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -67,6 +68,20 @@ fun RecipePage(
         } ?: run {
             // Show a loading indicator if recipeDetails is null
             CircularProgressIndicator()
+        }
+
+        // Botón para navegar hacia atrás
+        Button(
+            onClick = {
+                // Utiliza el NavHostController para navegar hacia atrás
+                navController.popBackStack()
+            },
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp)
+                .padding(top = 16.dp)
+        ) {
+            Text("Back to List")
         }
     }
 }
